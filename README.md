907b116dc8be5917c7dac48a2e3dfdafed02e1aa
hanhui <1018967523@qq.com>
2024-10-15 23:48:33 +0800

    修改备注，启动项目

---
+++ README.md
@@ -0,0 +1,10 @@
+## 启动步骤
+
+1. 最外层pnpm install
+2. 最外层执行 pnpm run docs:dev
+3. 浏览器打开 pnpm run docs:dev 构建的执行链接
+4. 进入到packages下面的各个项目中执行pnpm run dev 
+
+## 打包
+
+TODO
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,7 +4,7 @@ import '../../public/css/custom-style.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import VcComponents from '@much-more/vc-element-plus'
-import { globals } from '@/.vitepress/components'
+import { globals } from '../components'
 
 export default {
   ...Theme,
